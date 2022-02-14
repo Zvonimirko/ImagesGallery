8af3b77c8399b94a2e32e8bb21e3e6e81cfb35dc
Zvonimir Horvat <zvonimirhorvat@048b638c-06b5-45c7-87ed-af6a8746d923.fritz.box>
2022-02-14 15:28:09 +0100

    Added some packages for linting

--- api/main.py
+++ api/main.py
@@ -6,11 +6,11 @@ from flask_cors import CORS
 import requests
 import os
 
-load_dotenv(dotenv_path='./.env.local')
+load_dotenv(dotenv_path="./.env.local")
 
-UNSPLASH_KEY=os.environ.get("UNSPLASH_KEY", '')
-UNSPLASH_URL=os.environ.get("UNSPLASH_URL", '')
-DEBUG_MODE=bool(os.environ.get("DEBUG_MODE", True))
+UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
+UNSPLASH_URL = os.environ.get("UNSPLASH_URL", "")
+DEBUG_MODE = bool(os.environ.get("DEBUG_MODE", True))
 
 if not UNSPLASH_KEY:
     raise EnvironmentError("Please create .env.local file and insert UNSPLASH_KEY")
@@ -19,28 +19,28 @@ app = Flask(__name__)
 CORS(app)
 
 app.config["DEBUG"] = DEBUG_MODE
-    
-@app.route("/") 
+
+
+@app.route("/")
 def hello():
     return "Hello, World!"
 
-@app.route('/new-images')
+
+@app.route("/new-images")
 def new_image():
     word = request.args.get("query")
-    
+
     headers = {
         "Authorization": f"Client-ID {UNSPLASH_KEY}",
         "Accept-Version": "v1",
-        "Access-Control-Allow-Origin": "*"
+        "Access-Control-Allow-Origin": "*",
     }
-    params = {
-        "query": word,
-        "per_page": 20
-    }
+    params = {"query": word, "per_page": 20}
     response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
-    
+
     data = response.json()
     return data
+
 
 if __name__ == "__main__":
     app.run(port=5050)
