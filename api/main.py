75fd08b1aafa303c3e765eac861ee38202547df4
Zvonimir Horvat <zvonimirhorvat@550a66f2-cd89-452e-ae71-2c0a43aff9c4.fritz.box>
2022-02-14 13:56:57 +0100

    Changed url to flask be, enabled CORS of flask

--- api/main.py
+++ api/main.py
@@ -1,7 +1,8 @@
 # save this as app.py
-from pickle import TRUE
 from flask import Flask, request
 from dotenv import load_dotenv
+from flask_cors import CORS
+
 import requests
 import os
 
@@ -15,6 +16,7 @@ if not UNSPLASH_KEY:
     raise EnvironmentError("Please create .env.local file and insert UNSPLASH_KEY")
 
 app = Flask(__name__)
+CORS(app)
 
 app.config["DEBUG"] = DEBUG_MODE
     
@@ -28,7 +30,8 @@ def new_image():
     
     headers = {
         "Authorization": f"Client-ID {UNSPLASH_KEY}",
-        "Accept-Version": "v1"
+        "Accept-Version": "v1",
+        "Access-Control-Allow-Origin": "*"
     }
     params = {
         "query": word,
